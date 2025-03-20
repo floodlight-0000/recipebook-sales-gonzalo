154a7800e3e7677102713d51f209e6cf764bbfc2
floodlight <go.miguel65@gmail.com>
2025-03-20 13:49:14 +0800

    add code for new classes to admin.py

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient, Ingredient
 
-# Register your models here.
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
