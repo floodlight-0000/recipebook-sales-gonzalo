907f7fbb317989aa24339efc9b76e93e3141b2ee
floodlight <go.miguel65@gmail.com>
2025-03-20 15:42:52 +0800

    remove extra url pattern; begin implementing use of models

--- ledger/admin.py
+++ ledger/admin.py
@@ -3,12 +3,15 @@ from .models import Recipe, RecipeIngredient, Ingredient
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
+    search_fields = ('quantity', 'ingredient', 'recipe')
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
+    search_fields = ('name', )
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
+    search_fields = ('name', )
 
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
